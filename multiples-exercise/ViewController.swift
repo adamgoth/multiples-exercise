0f541d057ee5442f0418c673670d6b36acf9b018
Adam Goth <adamgoth@gmail.com>
2016-06-19 21:17:06 -0600

    Adds outlets and actions for functionality

--- multiples-exercise/ViewController.swift
+++ multiples-exercise/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Properties
+    var addNumber: Int = 0
+    var currentTotal: Int = 0
+    var previousTotal: Int = 0
+    
+    //Outlets
+    @IBOutlet weak var multiplesText: UIImageView!
+    @IBOutlet weak var textInput: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var numberText: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    
+    //Actions
+    @IBAction func hitPlayButton(sender: UIButton) {
+        if textInput.text != nil && textInput.text != "" {
+            textInput.hidden = true
+            playButton.hidden = true
+            addButton.hidden = false
+            numberText.hidden = false
+            
+            addNumber = Int(textInput.text!)!
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func pressAdd(sender: UIButton) {
+        numberText.text = "\(previousTotal) + \(addNumber) = \(previousTotal + addNumber)"
+        
+        previousTotal = currentTotal
+        currentTotal = addNumber + previousTotal
+        
     }
-
-
 }
 
